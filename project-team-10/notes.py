190a78ca3c35ae18d585a46a467d3bf355a943a0
Denys Filichkin <denys.philichkin@gmail.com>
2023-04-30 17:45:22 +0300

    add

--- project-team-10/notes.py
+++ project-team-10/notes.py
@@ -1,6 +1,12 @@
 # May the force be with you
+import functools
+import os
+from pathlib import Path
+
+
 class Notes:
    
+   @staticmethod
    def add_note():
       ...
 
@@ -23,8 +29,44 @@ class Notes:
       ...
 
 class CLINotes:
+   
+
+   def is_exist(note):
+      filename = note + ".txt"
+      full_path = os.path.join(Path().resolve(), "notes", filename)
+      if os.path.exists(full_path):
+         return True
+      return False
+   
+
+   def command_error_handler(func):
+      @functools.wraps(func)
+
+      def wrapper(*args):
+         try:
+            return func(*args)
+         except (ValueError, KeyError, Exception) as err:
+            return str(err)
+         
+      return wrapper
+
+   @staticmethod
    def help_handler():
-      ...
+      return ("""
+    You can use the following commands for your notebook:
+    - add note -> to create new note and save into folder 'notes';
+    - read note -> to open indicated note and read text inside;
+    - delete note -> to delete indicated note from the folder;
+    - find by tag -> to find all notes that are matched with this tag;
+    - find by name -> to find notes that are matched with this name;
+    - show all notes -> to show list of notes that were saved in folder;
+    - add tag -> to include additional tag to existing note;
+    - add text -> to include additional text to existing note;
+    - change tag -> to change existing tag in note (recommend to read note first);
+    - change text -> to change existing text in note (recommend to read note first);
+    - delete tag -> to delete existing tag in note (recommend to read note first);
+    - delete text -> to delete existing text in note (recommend to read note first);
+    """)
 
    def add_note_handler():
       ...
@@ -62,7 +104,7 @@ class CLINotes:
    def delete_text_handler():
       ...
 
-   notes_commands_dict = {
+   commands_dict = {
         "help": help_handler,
 
         "add note": add_note_handler,
@@ -82,7 +124,42 @@ class CLINotes:
         }
    
    def run_notes():
-      ...
-   
+      folder = os.path.join(Path().resolve(), "notes")
+
+      if not os.path.exists(folder):
+         os.mkdir(folder)
+
+      print("Hello! I'm here to assist you with your notes in your notebook.")
+      print("You could enter exact commands if you already know them.\n"
+              "Or please use:\n"
+              "  help -> to see whole list of commands\n"
+              "  exit -> to finish work with your notebook")
+      
+      while True:
+         command = input("You haven't entered command: ").lower().strip()
+
+         if command == "exit":
+            return"\nThanks, see you soon!!!\n"
+         
+         if command in CLINotes.commands_dict.keys():
+            handler = CLINotes.commands_dict[command]
+            answer = handler()
+            print(answer)
+
+         else:
+            commands_list = []
+            for k in CLINotes.commands_dict.keys():
+                for item in k.split():
+                  if command in item:
+                      commands_list.append(k)
+                      break
+                  
+            if commands_list:
+                print("What you have mean entered these commands: ")
+                print(*commands_list, sep=", ")
+            else:
+                print("Incorrect input.\nPlease check and enter correct command (or 'help' or 'exit').")
+         
+         
 if __name__ == '__main__':
    CLINotes.run_notes()
